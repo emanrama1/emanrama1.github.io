9b776db4eb27ac071beede0e2ef098d633178090
emanrama1 <eymantaleb2001@gmail.com>
2024-01-28 12:40:30 +0300

    Create script.js
    j

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('eemmmaaannnssshhhiii!! whhheeerrreee rrr uuu  here i am here iam what do u do');
+        
+        let accountBalance=4;
+
+        function changeAccountBalance(newAmount){
+            accountBalance=accountBalance+newAmount;
+            console.log(accountBalance);
+        };
