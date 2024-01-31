871322d2fbdccfdc30ea0c5b0367c2d5d095d673
emanrama1 <eymantaleb2001@gmail.com>
2024-01-31 14:34:32 +0300

    Beta test

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log('eemmmaaannnssshhhiii!! whhheeerrreee rrr uuu  here i am here iam what do u do');
-        
-        let accountBalance=4;
-
-        function changeAccountBalance(newAmount){
-            accountBalance=accountBalance+newAmount;
-            console.log(accountBalance);
-        };
